8e13376f72d78cacf61d286521fc02bdb1ff1d64
fassabilf <fassabilf@gmail.com>
2025-03-07 19:30:50 +0700

    [REFACTOR] Move PaymentService implementation to PaymentServiceImpl

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
@@ -2,34 +2,11 @@ package id.ac.ui.cs.advprog.eshop.service;
 
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
-import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
-import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
 
-@Service
-public class PaymentService {
-    private final PaymentRepository paymentRepository;
-
-    public PaymentService(PaymentRepository paymentRepository) {
-        this.paymentRepository = paymentRepository;
-    }
-
-    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        if (order == null || method == null || paymentData == null) {
-            throw new IllegalArgumentException("Order, method, and payment data must not be null");
-        }
-
-        Payment payment = new Payment(order.getId(), method, paymentData);
-        paymentRepository.save(payment);
-        return payment;
-    }
-
-    public Payment getPayment(String paymentId) {
-        return paymentRepository.findById(paymentId);
-    }
-
-    public List<Payment> getAllPayments() {
-        return paymentRepository.getAllPayments();
-    }
+public interface PaymentService {
+    Payment addPayment(Order order, String method, Map<String, String> paymentData);
+    Payment getPayment(String paymentId);
+    List<Payment> getAllPayments();
 }
