7cbd8e564ec65ee6d24767922899dc82b2cfd838
fassabilf <fassabilf@gmail.com>
2025-03-07 17:12:09 +0700

    [RED] Add tests for Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -1,7 +1,15 @@
 package id.ac.ui.cs.advprog.eshop.controller;
-
+import org.springframework.stereotype.Controller;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +19,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/car")
-class CarController{
+public class CarController{
     @Autowired
     private CarService carService;
 
@@ -25,7 +33,7 @@ class CarController{
     @PostMapping("/createCar")
     public String createCarPost(@ModelAttribute Car car, Model model) {
         carService.create(car);
-        return "redirect:listCar";
+        return "redirect:/car/listCar";
     }
 
     @GetMapping("/listCar")
@@ -45,12 +53,12 @@ class CarController{
     @PostMapping("/editCar")
     public String editCarPost(@ModelAttribute Car car, Model model) {
         carService.update(car.getCarId(), car);
-        return "redirect:listCar";
+        return "redirect:/car/listCar";
     }
 
     @PostMapping("/deleteCar")
     public String deleteCar(@RequestParam("carId") String carId) {
         carService.deleteCarById(carId);
-        return "redirect:listCar";
+        return "redirect:/car/listCar";
     }
 }
